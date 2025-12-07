4d1f1dec45114492282c438f070e1b85b5050515
ihakhverdiyev2019 <ihakhverdiyev2019@gmail.com>
2025-12-07 19:12:11 +0100

    Refactor: Add multi-datasource configuration for admin_db and public_db
    - Added AdminDataSourceConfig for User, Device, Subscription, AnalysisRequest (admin_db)
    - Added PublicDataSourceConfig for Deal entities (public_db - read-only)
    - Updated application.yml to use admin_db as primary datasource
    - Updated application-dev.yml with public-datasource configuration
    - DealRepository now uses public_db, other repositories use admin_db
    - Removed temporary scripts and documentation files

---
+++ src/src/main/java/com/rensights/config/AdminDataSourceConfig.java
@@ -0,0 +1,80 @@
+package com.rensights.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.rensights.model.AnalysisRequest;
+import com.rensights.model.Device;
+import com.rensights.model.Subscription;
+import com.rensights.model.User;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    basePackages = "com.rensights.repository",
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {com.rensights.repository.DealRepository.class})
+    },
+    entityManagerFactoryRef = "adminEntityManagerFactory",
+    transactionManagerRef = "adminTransactionManager"
+)
+public class AdminDataSourceConfig {
+
+    @Primary
+    @Bean(name = "adminDataSourceProperties")
+    @ConfigurationProperties("spring.datasource")
+    public DataSourceProperties adminDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Primary
+    @Bean(name = "adminDataSource")
+    public DataSource adminDataSource(@Qualifier("adminDataSourceProperties") DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder()
+            .type(HikariDataSource.class)
+            .build();
+    }
+
+    @Primary
+    @Bean(name = "adminEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean adminEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("adminDataSource") DataSource dataSource) {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "validate");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.show_sql", "true");
+        
+        return builder
+            .dataSource(dataSource)
+            .packages(User.class, Device.class, Subscription.class, AnalysisRequest.class)
+            .persistenceUnit("admin")
+            .properties(properties)
+            .build();
+    }
+
+    @Primary
+    @Bean(name = "adminTransactionManager")
+    public PlatformTransactionManager adminTransactionManager(
+            @Qualifier("adminEntityManagerFactory") LocalContainerEntityManagerFactoryBean adminEntityManagerFactory) {
+        return new JpaTransactionManager(adminEntityManagerFactory.getObject());
+    }
+}
+
---
+++ src/src/main/java/com/rensights/config/PublicDataSourceConfig.java
@@ -0,0 +1,72 @@
+package com.rensights.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.rensights.model.Deal;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+    basePackages = "com.rensights.repository",
+    includeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {com.rensights.repository.DealRepository.class})
+    },
+    entityManagerFactoryRef = "publicEntityManagerFactory",
+    transactionManagerRef = "publicTransactionManager"
+)
+public class PublicDataSourceConfig {
+
+    @Bean(name = "publicDataSourceProperties")
+    @ConfigurationProperties("spring.public-datasource")
+    public DataSourceProperties publicDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean(name = "publicDataSource")
+    public DataSource publicDataSource(@Qualifier("publicDataSourceProperties") DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder()
+            .type(HikariDataSource.class)
+            .build();
+    }
+
+    @Bean(name = "publicEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean publicEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("publicDataSource") DataSource dataSource) {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", "validate");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.show_sql", "true");
+        
+        return builder
+            .dataSource(dataSource)
+            .packages(Deal.class)
+            .persistenceUnit("public")
+            .properties(properties)
+            .build();
+    }
+
+    @Bean(name = "publicTransactionManager")
+    public PlatformTransactionManager publicTransactionManager(
+            @Qualifier("publicEntityManagerFactory") LocalContainerEntityManagerFactoryBean publicEntityManagerFactory) {
+        return new JpaTransactionManager(publicEntityManagerFactory.getObject());
+    }
+}
+
